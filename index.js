09f375770672a6f986314ebc658559daf0a5b7c9
Shryler <105290336+Shryler@users.noreply.github.com>
2022-07-16 15:37:46 +0200

    Changements

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const result = document.querySelector("#response");
 const form = document.querySelector("#form");
 let commune = [];
+let check = true;
 
 
 // Récupération des données sur l'API
 async function nomCommune(codePostal) {
+    // await fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${codePostal}`)
+    //     .then((res) => res.json())
+    //     .then((data) => (commune = data))
     await fetch(`https://apicarto.ign.fr/api/codes-postaux/communes/${codePostal}`)
+        .then(function (res) {
+            if (!res.ok) {
+                errorMsg.innerHTML = "Ce code postal n'existe pas !";
+                check = false;
+            }
+            return res;
+        })
         .then((res) => res.json())
-        .then((data) => (commune = data));
+        .then((data) => (commune = data))
+        check = true;
 }
 
 // Fonction avec .map pour écrire le résultat
 function communeDisplay() {
-    result.innerHTML = commune.map((commune) =>
-        `
-        <option>${commune.nomCommune}</option>
-        `
+    result.innerHTML = commune.map((commune) => {
+        if (check == false) {
+            return `
+        
+            <option>Commune</option>
+            `
+        } else {
+            return `
+                <option>${commune.nomCommune}</option>
+                `
+        }
+    }
     ).join("");
 }
 
@@ -26,12 +46,13 @@ form.addEventListener("input", (e) => {
     let match = e.target.value.match(/^(?:0[1-9]|[1-8]\d|9[0-8])\d{3}$/);
     if (match !== null) {
         nomCommune(e.target.value).then(() => communeDisplay());
+        errorMsg.innerHTML = "";
     }
     else if (match == null) {
         result.innerHTML = "<option>Commune</option>";
+        errorMsg.innerHTML = "";
     }
     else {
         alert("Erreur");
     }
 });
-
